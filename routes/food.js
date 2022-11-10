11ac670af4a8d598e2aecd1c925a43c4cd4db2f5
Chilex23 <onumaegbuchilex@gmail.com>
2022-11-10 21:31:06 +0100

    Added new test cases for the food endpoints

--- routes/food.js
+++ routes/food.js
@@ -21,11 +21,13 @@ export let router = express.Router();
 router.use(passport.authenticate("jwt", { session: false }));
 router.get("/", getAllFoodController);
 router.get("/:id", getFoodController);
-router.get("/category/:category", getFoodsByCategoryController);
-router.post("/category/add", restrictTo("Admin"), addCategoryController);
 router.post("/add", restrictTo("Admin"), upload.single("foodImage"), addFoodController);
 router.patch("/update/:id", restrictTo("Admin"), updateFoodController);
 router.delete("/delete/:id", restrictTo("Admin"), deleteFoodController);
+
+router.get("/category/:category", getFoodsByCategoryController);
+router.post("/category/add", restrictTo("Admin"), addCategoryController);
+
 router.post("/reviews/add/:id", addReviewController);
 router.patch("/reviews/update/:id", updateReviewController);
 router.get("/reviews/:id", getReviewsContoller);
